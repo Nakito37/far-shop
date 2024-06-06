5ef75f3f59abb503af96c56a2e0fc3b8749171ca
Nakito37 <mazin37.fa@gmail.com>
2024-06-06 08:03:57 -0600

    login y registro

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -10,10 +10,27 @@ export class LoginComponent {
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required]
+    password: ['', Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})')]
   });
 
   constructor(private fb: FormBuilder){
-    
+   
+  }
+
+  get email(){
+      return this.loginForm.controls['email'];
+  }
+
+  get password(){
+    return this.loginForm.controls['password'];
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario válido:', this.loginForm.value);
+      
+    } else {
+      console.log('Formulario no válido');
+    }
   }
 }
